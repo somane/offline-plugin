01b7e9fb593f0ebd8cf05b11af2942b372ac589e
William Durand <will+git@drnd.me>
2016-03-29 15:32:48 +0200

    Redirect to '/' for all URLs with document id
    Also, handle cache/fetch better

--- src/misc/sw-template.js
+++ src/misc/sw-template.js
@@ -1,9 +1,11 @@
-function WebpackServiceWorker(params) {
-  const scopeURL = new URL(registration.scope);
+'use strict';
 
-  const strategy = params.strategy;
-  const assets = params.assets;
-  const tagMap = {
+function WebpackServiceWorker(params) {
+  var scopeURL = new URL(registration.scope);
+
+  var strategy = params.strategy;
+  var assets = params.assets;
+  var tagMap = {
     all: params.version,
     // Hash is included in output file, but not used in cache name,
     // this allows updating only changed files in `additional` section and
@@ -12,36 +14,35 @@ function WebpackServiceWorker(params) {
     hash: params.hash
   };
 
-  const CACHE_PREFIX = params.name;
-  const CACHE_TAG = tagMap[strategy];
-  const CACHE_NAME = CACHE_PREFIX + ':' + CACHE_TAG;
+  var CACHE_PREFIX = params.name;
+  var CACHE_TAG = tagMap[strategy];
+  var CACHE_NAME = CACHE_PREFIX + ':' + CACHE_TAG;
 
   if (params.relativePaths) {
     mapAssets();
   }
 
-  const allAssets = [].concat(assets.main, assets.additional, assets.optional);
+  var allAssets = [].concat(assets.main, assets.additional, assets.optional);
 
-  self.addEventListener('install', (event) => {
+  self.addEventListener('install', function (event) {
     console.log('[SW]:', 'Install event');
 
-    const installing = cacheAssets('main').then(cacheAdditional);
+    var installing = cacheAssets('main').then(cacheAdditional);
     event.waitUntil(installing);
   });
 
-
-  self.addEventListener('activate', (event) => {
+  self.addEventListener('activate', function (event) {
     console.log('[SW]:', 'Activate event');
 
     // Delete all assets which start with CACHE_PREFIX and
     // is not current cache (CACHE_NAME)
-    let deletion = deleteObsolete();
+    var deletion = deleteObsolete();
 
     if (strategy === 'changed') {
       deletion = deletion.then(deleteChanged);
     }
 
-    event.waitUntil(deletion.then(() => {
+    event.waitUntil(deletion.then(function () {
       if (self.clients && self.clients.claim) {
         return self.clients.claim();
       }
@@ -63,10 +64,10 @@ function WebpackServiceWorker(params) {
   }
 
   function cacheAssets(section) {
-    return caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(assets[section]).then(() => {
+    return caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(assets[section]).then(function () {
         console.groupCollapsed('[SW]:', 'Cached assets: ' + section);
-        assets[section].forEach((asset) => {
+        assets[section].forEach(function (asset) {
           console.log('Asset:', asset);
         });
         console.groupEnd();
@@ -75,54 +76,54 @@ function WebpackServiceWorker(params) {
   }
 
   function cacheChanged() {
-    let cache;
+    var cache = undefined;
 
-    return caches.open(CACHE_NAME).then((_cache) => {
+    return caches.open(CACHE_NAME).then(function (_cache) {
       cache = _cache;
       return _cache.keys();
-    }).then(keys => {
-      const paths = keys.map(req => {
+    }).then(function (keys) {
+      var paths = keys.map(function (req) {
         return new URL(req.url).pathname;
       });
 
-      const changed = assets.additional.filter((path) => {
+      var changed = assets.additional.filter(function (path) {
         return paths.indexOf(path) === -1;
       });
 
       if (!changed.length) return;
 
       console.group('[SW]:', 'Caching changed assets');
-      changed.map((path) => {
+      changed.map(function (path) {
         console.log('Asset:', path);
         return new Request(path);
-      }).map((req) => {
-        return fetch(req).then((res) => {
+      }).map(function (req) {
+        return fetch(req).then(function (res) {
           return cache.put(req, res);
         });
       });
       console.groupEnd();
-    })
+    });
   }
 
   function deleteObsolete() {
-    return caches.keys().then((names) => {
-      return Promise.all(names.map((name) => {
+    return caches.keys().then(function (names) {
+      return Promise.all(names.map(function (name) {
         if (name === CACHE_NAME || name.indexOf(CACHE_PREFIX) !== 0) return;
         console.log('[SW]:', 'Delete cache:', name);
-        return caches.delete(name);
+        return caches['delete'](name);
       }));
     });
   }
 
   function deleteChanged() {
-    let cache;
+    var cache = undefined;
 
-    return caches.open(CACHE_NAME).then((_cache) => {
+    return caches.open(CACHE_NAME).then(function (_cache) {
       cache = _cache;
       return _cache.keys();
-    }).then((keys) => {
-      let deletion = keys.filter((req) => {
-        const url = new URL(req.url);
+    }).then(function (keys) {
+      var deletion = keys.filter(function (req) {
+        var url = new URL(req.url);
 
         if (allAssets.indexOf(url.pathname) === -1) {
           req._pathname = url.pathname;
@@ -132,9 +133,9 @@ function WebpackServiceWorker(params) {
       if (!deletion.length) return;
 
       console.group('[SW]:', 'Deleting changed assets');
-      deletion = deletion.map((req) => {
+      deletion = deletion.map(function (req) {
         console.log('Asset:', req._pathname);
-        return cache.delete(req);
+        return cache['delete'](req);
       });
       console.groupEnd();
 
@@ -142,16 +143,12 @@ function WebpackServiceWorker(params) {
     });
   }
 
-  self.addEventListener('fetch', (event) => {
-    const url = new URL(event.request.url);
+  self.addEventListener('fetch', function (event) {
+    var url = new URL(event.request.url);
 
     // Match only same origin and known caches
     // otherwise just perform fetch()
-    if (
-      event.request.method !== 'GET' ||
-      url.origin !== location.origin ||
-      allAssets.indexOf(url.pathname) === -1
-    ) {
+    if (event.request.method !== 'GET' || url.origin !== location.origin || allAssets.indexOf(url.pathname) === -1) {
       if (DEBUG) {
         console.log('[SW]:', 'Path [' + url.pathname + '] does not match any assets');
       }
@@ -161,23 +158,28 @@ function WebpackServiceWorker(params) {
         event.respondWith(fetch(event.request));
       }
 
+      if (/\/[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}/.test(url.pathname)) {
+        event.respondWith(
+          caches.match('/', {
+            cacheName: CACHE_NAME
+          }).then(function (response) {
+            if (response) {
+              return response;
+            }
+
+            return fetch('/').then(function (response) {
+              return response;
+            });
+          })
+        );
+      }
+
       return;
     }
 
-    // if asset is from main entry read it directly from the cache
-    if (assets.main.indexOf(url.pathname) !== -1) {
-      event.respondWith(
-        caches.match(event.request, {
-          cacheName: CACHE_NAME
-        })
-      );
-
-      return;
-    }
-
-    const resource = caches.match(event.request, {
+    var resource = caches.match(event.request, {
       cacheName: CACHE_NAME
-    }).then((response) => {
+    }).then(function (response) {
       if (response) {
         if (DEBUG) {
           console.log('[SW]:', 'Path [' + url.pathname + '] from cache');
@@ -187,10 +189,8 @@ function WebpackServiceWorker(params) {
       }
 
       // Load and cache known assets
-      return fetch(event.request.clone()).then((response) => {
-        if (
-          !response || response.status !== 200 || response.type !== 'basic'
-        ) {
+      return fetch(event.request.clone()).then(function (response) {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
           if (DEBUG) {
             console.log('[SW]:', 'Path [' + url.pathname + '] wrong response');
           }
@@ -202,25 +202,31 @@ function WebpackServiceWorker(params) {
           console.log('[SW]:', 'Path [' + url.pathname + '] fetched');
         }
 
-        const responseClone = response.clone();
+        var responseClone = response.clone();
 
-        caches.open(CACHE_NAME).then((cache) => {
+        caches.open(CACHE_NAME).then(function (cache) {
           return cache.put(event.request, responseClone);
-        }).then(() => {
+        }).then(function () {
           console.log('[SW]:', 'Cache asset: ' + url.pathname);
+        }).catch(function () {
+          // this happens when one manually deletes the cache :(
         });
 
         return response;
+      }).catch(function (err) {
+        console.log('[SW]: Error fetch - ' + err);
       });
+    }).catch(function (err) {
+      console.log('[SW]: Error caches.match - ' + err);
     });
 
     event.respondWith(resource);
   });
 
   function mapAssets() {
-    Object.keys(assets).forEach((key) => {
-      assets[key] = assets[key].map((path) => {
-        const pathURL = new URL(scopeURL.origin + scopeURL.pathname + path);
+    Object.keys(assets).forEach(function (key) {
+      assets[key] = assets[key].map(function (path) {
+        var pathURL = new URL(scopeURL.origin + scopeURL.pathname + path);
         return pathURL.pathname;
       });
     });
